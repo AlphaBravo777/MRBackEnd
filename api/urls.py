a6a2ba05633d7d0ff71e26877fcdf4dba79bb00b
Gray <gray@gmail.com>
2018-05-31 08:08:26 +0200

    Working registration but no Login, just before changing Auth completely

--- api/urls.py
+++ api/urls.py
@@ -10,7 +10,8 @@ router.register(r'users', views.UserViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('api/users/', views.UserCreate.as_view(), name='account-create'),
+    path('api/users/register', views.UserCreate.as_view(), name='account-create'),
+    path('api/users/login', views.UserLogin.as_view(), name='account-login'),
     url(r'^', include(router.urls)),
     #url(r'users^$', views.UserCreate.as_view(), name='account-create'),
     #url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
--- api/views.py
+++ api/views.py
@@ -31,3 +31,20 @@ class UserCreate(APIView):
                 return Response(json, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserLogin(APIView):
+    """ 
+    Login the user. 
+    """
+
+    def get(self, request, format='json'):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                # token = Token.objects.create(user=user)
+                # json = serializer.data
+                # json['token'] = token.key
+                return Response(json, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
