3e5313fda8e55f064c07f4fd504481fdf17cc683
Gray <gray@gmail.com>
2018-05-31 12:24:46 +0200

    Working just before varifying token

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from rest_framework import routers
 from api import views
 from . import views
+from rest_framework_jwt.views import refresh_jwt_token
+from rest_framework_jwt.views import verify_jwt_token
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -15,4 +17,10 @@ urlpatterns = [
     url(r'^', include(router.urls)),
     #url(r'users^$', views.UserCreate.as_view(), name='account-create'),
     #url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    #url(r'^', include('rest_auth.urls')),
+    url(r'^registration/', include('rest_auth.registration.urls')),
+    url(r'^rest-auth/', include('rest_auth.urls')),
+    url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
+    url(r'^refresh-token/', refresh_jwt_token),
+    url(r'^api-token-verify/', verify_jwt_token),
 ]
--- meatriteApi/settings.py
+++ meatriteApi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import datetime
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,10 +38,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'api',
-    'rest_framework.authtoken', #This was added for a test
+    'django.contrib.sites',
     'corsheaders',
+    'rest_framework',
+    'rest_framework.authtoken', #This was added for a test
+    'rest_auth',
+    'rest_auth.registration',
+    'allauth',
+    'allauth.account',
+    'api',
+
 ]
 
 MIDDLEWARE = [
@@ -57,11 +64,25 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'meatriteApi.urls'
 
+# Make JWT Auth the default authentication mechanism for Django
 REST_FRAMEWORK = {
-  'DEFAULT_AUTHENTICATION_CLASSES': (
-      'rest_framework.authentication.TokenAuthentication',
-    )
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+    ),
 }
+
+# Enables django-rest-auth to use JWT tokens instead of regular tokens.
+REST_USE_JWT = True
+
+# Configure the JWTs to expire after 1 hour, and allow users to refresh near-expiration tokens
+JWT_AUTH = {
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(hours=1),
+    'JWT_ALLOW_REFRESH': True,
+}
+
+SITE_ID = 1
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 CORS_ORIGIN_ALLOW_ALL = True
 #CORS_ORIGIN_WHITELIST = (
@@ -70,6 +91,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 #    'localhost:8000',
 #    '127.0.0.1:9000'
 #)
+
 
 TEMPLATES = [
     {
