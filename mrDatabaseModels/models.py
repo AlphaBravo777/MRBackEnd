184edb153d2a8c9f6462f5a68d3653e3b244e3a5
Gray <gray@gmail.com>
2018-06-06 10:43:09 +0200

    Correctly displaying products backend and frontend

--- mrDatabaseModels/models.py
+++ mrDatabaseModels/models.py
@@ -1,5 +1,22 @@
 from django.db import models
 
+class Packaging(models.Model):
+    packagingType = models.CharField(max_length=255, blank=True, null=True)
+
+    class Meta:
+        managed = True
+        db_table = 'tbl_packaging'
+
+class ProductBrands(models.Model):
+    brand = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.brand
+
+    class Meta: 
+        managed = True
+        db_table = 'tbl_productbrands'
+
 class Batchgroups(models.Model):
     batchname = models.CharField(db_column='batchName', unique=True, max_length=255)  # Field name made lowercase.
     ranking = models.IntegerField(unique=True)
@@ -14,12 +31,13 @@ class Batchgroups(models.Model):
 class Productlist(models.Model):
     productid = models.CharField(db_column='productID', unique=True, max_length=20)  # Field name made lowercase.
     proddescription = models.CharField(db_column='prodDescription', max_length=255, blank=True, null=True)  # Field name made lowercase.
-    packaging = models.IntegerField(blank=True, null=True)
+    packaging = models.ForeignKey(Packaging, on_delete=models.CASCADE, blank=True, null=True)
     unitweight = models.FloatField(db_column='unitWeight', blank=True, null=True)  # Field name made lowercase.
     packageweight = models.FloatField(db_column='packageWeight', blank=True, null=True)  # Field name made lowercase.
     productonhold = models.IntegerField(db_column='productOnHold', blank=True, null=True)  # Field name made lowercase.
     batchgroup = models.ForeignKey(Batchgroups, on_delete=models.CASCADE)
     batchranking = models.IntegerField(db_column='batchRanking', blank=True, null=True)  # Field name made lowercase.
+    brand = models.ForeignKey(ProductBrands, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.productid
@@ -27,3 +45,11 @@ class Productlist(models.Model):
     class Meta:
         managed = True
         db_table = 'tbl_productlist'
+
+class TblDeliveryroutes(models.Model):
+    routes = models.CharField(unique=True, max_length=255)
+    loadingday = models.IntegerField(db_column='loadingDay', blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = True
+        db_table = 'tbl_deliveryroutes'
--- mrDatabaseModels/serializers.py
+++ mrDatabaseModels/serializers.py
@@ -5,12 +5,15 @@ from .models import Productlist
 # This serializer is made to give a list through of all the products that we have
 class ProductListSerializer(serializers.ModelSerializer):
     """Serializer to map the Model instance into JSON format."""
+    # description = serializers.CharField(source='proddescription')
+    brand = serializers.SlugRelatedField(read_only=True, slug_field='brand')
+    packaging = serializers.SlugRelatedField(read_only=True, slug_field='packagingType')
 
     class Meta:
         """Meta class to map serializer's fields with the model fields."""
         # Then name of the model that you want to serialize (Remember to import the model)
         model = Productlist
         # All the fields that you want to show/work with
-        fields = ('productid', 'proddescription', 'batchgroup', 'batchranking', 'packaging')
+        fields = ('brand', 'packaging', 'unitweight', 'productid')
         # All the field that you want to be read only (Can not be changed)
-        read_only_fields = ('packaging', 'batchgroup')
+        read_only_fields = ('batchgroup', 'batchranking')
