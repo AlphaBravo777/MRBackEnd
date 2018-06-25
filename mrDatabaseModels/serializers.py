726b2fd23f6f0c9ce9debdbd4484bb6bb5c9e7bb
Gray <gray@gmail.com>
2018-06-25 07:42:48 +0200

    Working stock-take before adding single stock add items

--- mrDatabaseModels/serializers.py
+++ mrDatabaseModels/serializers.py
@@ -8,12 +8,13 @@ class ProductListSerializer(serializers.ModelSerializer):
     # description = serializers.CharField(source='proddescription')
     brand = serializers.SlugRelatedField(read_only=True, slug_field='brand')
     packaging = serializers.SlugRelatedField(read_only=True, slug_field='packagingType')
+    batchgroup = serializers.SlugRelatedField(read_only=True, slug_field='batchname')
 
     class Meta:
         """Meta class to map serializer's fields with the model fields."""
         # Then name of the model that you want to serialize (Remember to import the model)
         model = Productlist
         # All the fields that you want to show/work with
-        fields = ('brand', 'packaging', 'unitweight', 'productid')
+        fields = ('brand', 'packaging', 'unitweight', 'productid','batchgroup')
         # All the field that you want to be read only (Can not be changed)
-        read_only_fields = ('batchgroup', 'batchranking')
+        read_only_fields = ('batchranking',)
