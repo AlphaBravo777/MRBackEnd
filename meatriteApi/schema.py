5e45f5cfccf704c4dab3cf3969454a5c59331b73
Gray <jgreyling7@gmail.com>
2020-03-18 10:19:20 +0200

    Added Product service to graphql schema

--- meatriteApi/schema.py
+++ meatriteApi/schema.py
@@ -8,8 +8,9 @@ import mrAdminOffice.schema as Office
 import mrStock.schema as Stock
 import graphqlMicroservicesEndpoints.microServiceOrderSchema as OrderService
 import graphqlMicroservicesEndpoints.microServiceAccountSchema as AccountService
+import graphqlMicroservicesEndpoints.microServiceProductSchema as ProductService
 
-class Query(Database.Query, Core.Query, Office.Query, Stock.Query, AccountService.Query, OrderService.Query, graphene.ObjectType):
+class Query(Database.Query, Core.Query, Office.Query, Stock.Query, AccountService.Query, OrderService.Query, ProductService.Query, graphene.ObjectType):
     # This class extends all abstract apps level Queries and graphene.ObjectType
     pass
 
